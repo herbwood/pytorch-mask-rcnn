d9a9ff87fdce7186268f8c38ae92c237e0c4cf89
June Kwon <herbwood27@gmail.com>
2021-03-16 21:28:55 +0900

    changed whole things

--- transform.py
+++ transform.py
@@ -5,19 +5,18 @@ from torch.nn import functional as F
 import torchvision
 from typing import List, Tuple, Dict, Optional
 
-from utils import ImageList
+from image_list import ImageList
 from roi_heads import paste_masks_in_image
 
 
-def _resize_image_and_masks(image, self_min_size, self_max_size, target):
-
-    im_shape = torch.tensor(image.shape[-2:])
-    min_size = float(torch.min(im_shape))
-    max_size = float(torch.max(im_shape))
-    scale_factor = self_min_size / min_size
-
-    if max_size * scale_factor > self_max_size:
-        scale_factor = self_max_size / max_size
+@torch.jit.unused
+def _resize_image_and_masks_onnx(image, self_min_size, self_max_size, target):
+    # type: (Tensor, float, float, Optional[Dict[str, Tensor]]) -> Tuple[Tensor, Optional[Dict[str, Tensor]]]
+    from torch.onnx import operators
+    im_shape = operators.shape_as_tensor(image)[-2:]
+    min_size = torch.min(im_shape).to(dtype=torch.float32)
+    max_size = torch.max(im_shape).to(dtype=torch.float32)
+    scale_factor = torch.min(self_min_size / min_size, self_max_size / max_size)
 
     image = torch.nn.functional.interpolate(
         image[None], scale_factor=scale_factor, mode='bilinear', recompute_scale_factor=True,
@@ -33,7 +32,39 @@ def _resize_image_and_masks(image, self_min_size, self_max_size, target):
     return image, target
 
 
+def _resize_image_and_masks(image, self_min_size, self_max_size, target):
+    # type: (Tensor, float, float, Optional[Dict[str, Tensor]]) -> Tuple[Tensor, Optional[Dict[str, Tensor]]]
+    im_shape = torch.tensor(image.shape[-2:])
+    min_size = float(torch.min(im_shape))
+    max_size = float(torch.max(im_shape))
+    scale_factor = self_min_size / min_size
+    if max_size * scale_factor > self_max_size:
+        scale_factor = self_max_size / max_size
+    image = torch.nn.functional.interpolate(
+        image[None], scale_factor=scale_factor, mode='bilinear', recompute_scale_factor=True,
+        align_corners=False)[0]
+
+    if target is None:
+        return image, target
+
+    if "masks" in target:
+        mask = target["masks"]
+        mask = F.interpolate(mask[:, None].float(), scale_factor=scale_factor, recompute_scale_factor=True)[:, 0].byte()
+        target["masks"] = mask
+    return image, target
+
+
 class GeneralizedRCNNTransform(nn.Module):
+    """
+    Performs input / target transformation before feeding the data to a GeneralizedRCNN
+    model.
+
+    The transformations it perform are:
+        - input normalization (mean subtraction and std division)
+        - input / target resizing to match min_size / max_size
+
+    It returns a ImageList for the inputs, and a List[Dict[Tensor]] for the targets
+    """
 
     def __init__(self, min_size, max_size, image_mean, image_std):
         super(GeneralizedRCNNTransform, self).__init__()
@@ -50,6 +81,18 @@ class GeneralizedRCNNTransform(nn.Module):
                 ):
         # type: (...) -> Tuple[ImageList, Optional[List[Dict[str, Tensor]]]]
         images = [img for img in images]
+        if targets is not None:
+            # make a copy of targets to avoid modifying it in-place
+            # once torchscript supports dict comprehension
+            # this can be simplified as as follows
+            # targets = [{k: v for k,v in t.items()} for t in targets]
+            targets_copy: List[Dict[str, Tensor]] = []
+            for t in targets:
+                data: Dict[str, Tensor] = {}
+                for k, v in t.items():
+                    data[k] = v
+                targets_copy.append(data)
+            targets = targets_copy
         for i in range(len(images)):
             image = images[i]
             target_index = targets[i] if targets is not None else None
@@ -84,11 +127,15 @@ class GeneralizedRCNNTransform(nn.Module):
         std = torch.as_tensor(self.image_std, dtype=dtype, device=device)
         return (image - mean[:, None, None]) / std[:, None, None]
 
-
     def torch_choice(self, k):
+        # type: (List[int]) -> int
+        """
+        Implements `random.choice` via torch ops so it can be compiled with
+        TorchScript. Remove if https://github.com/pytorch/pytorch/issues/25803
+        is fixed.
+        """
         index = int(torch.empty(1).uniform_(0., float(len(k))).item())
         return k[index]
-
 
     def resize(self, image, target):
         # type: (Tensor, Optional[Dict[str, Tensor]]) -> Tuple[Tensor, Optional[Dict[str, Tensor]]]
@@ -96,7 +143,11 @@ class GeneralizedRCNNTransform(nn.Module):
         if self.training:
             size = float(self.torch_choice(self.min_size))
         else:
+            # FIXME assume for now that testing uses the largest scale
             size = float(self.min_size[-1])
+        if torchvision._is_tracing():
+            image, target = _resize_image_and_masks_onnx(image, size, float(self.max_size), target)
+        else:
             image, target = _resize_image_and_masks(image, size, float(self.max_size), target)
 
         if target is None:
@@ -106,8 +157,36 @@ class GeneralizedRCNNTransform(nn.Module):
         bbox = resize_boxes(bbox, (h, w), image.shape[-2:])
         target["boxes"] = bbox
 
+        if "keypoints" in target:
+            keypoints = target["keypoints"]
+            keypoints = resize_keypoints(keypoints, (h, w), image.shape[-2:])
+            target["keypoints"] = keypoints
         return image, target
 
+    # _onnx_batch_images() is an implementation of
+    # batch_images() that is supported by ONNX tracing.
+    @torch.jit.unused
+    def _onnx_batch_images(self, images, size_divisible=32):
+        # type: (List[Tensor], int) -> Tensor
+        max_size = []
+        for i in range(images[0].dim()):
+            max_size_i = torch.max(torch.stack([img.shape[i] for img in images]).to(torch.float32)).to(torch.int64)
+            max_size.append(max_size_i)
+        stride = size_divisible
+        max_size[1] = (torch.ceil((max_size[1].to(torch.float32)) / stride) * stride).to(torch.int64)
+        max_size[2] = (torch.ceil((max_size[2].to(torch.float32)) / stride) * stride).to(torch.int64)
+        max_size = tuple(max_size)
+
+        # work around for
+        # pad_img[: img.shape[0], : img.shape[1], : img.shape[2]].copy_(img)
+        # which is not yet supported in onnx
+        padded_imgs = []
+        for img in images:
+            padding = [(s1 - s2) for s1, s2 in zip(max_size, tuple(img.shape))]
+            padded_img = torch.nn.functional.pad(img, (0, padding[2], 0, padding[1], 0, padding[0]))
+            padded_imgs.append(padded_img)
+
+        return torch.stack(padded_imgs)
 
     def max_by_axis(self, the_list):
         # type: (List[List[int]]) -> List[int]
@@ -116,7 +195,6 @@ class GeneralizedRCNNTransform(nn.Module):
             for index, item in enumerate(sublist):
                 maxes[index] = max(maxes[index], item)
         return maxes
-
 
     def batch_images(self, images, size_divisible=32):
         # type: (List[Tensor], int) -> Tensor
@@ -138,13 +216,11 @@ class GeneralizedRCNNTransform(nn.Module):
 
         return batched_imgs
 
-
     def postprocess(self,
                     result,               # type: List[Dict[str, Tensor]]
                     image_shapes,         # type: List[Tuple[int, int]]
                     original_image_sizes  # type: List[Tuple[int, int]]
                     ):
-
         # type: (...) -> List[Dict[str, Tensor]]
         if self.training:
             return result
@@ -156,7 +232,39 @@ class GeneralizedRCNNTransform(nn.Module):
                 masks = pred["masks"]
                 masks = paste_masks_in_image(masks, boxes, o_im_s)
                 result[i]["masks"] = masks
+            if "keypoints" in pred:
+                keypoints = pred["keypoints"]
+                keypoints = resize_keypoints(keypoints, im_s, o_im_s)
+                result[i]["keypoints"] = keypoints
         return result
+
+    def __repr__(self):
+        format_string = self.__class__.__name__ + '('
+        _indent = '\n    '
+        format_string += "{0}Normalize(mean={1}, std={2})".format(_indent, self.image_mean, self.image_std)
+        format_string += "{0}Resize(min_size={1}, max_size={2}, mode='bilinear')".format(_indent, self.min_size,
+                                                                                         self.max_size)
+        format_string += '\n)'
+        return format_string
+
+
+def resize_keypoints(keypoints, original_size, new_size):
+    # type: (Tensor, List[int], List[int]) -> Tensor
+    ratios = [
+        torch.tensor(s, dtype=torch.float32, device=keypoints.device) /
+        torch.tensor(s_orig, dtype=torch.float32, device=keypoints.device)
+        for s, s_orig in zip(new_size, original_size)
+    ]
+    ratio_h, ratio_w = ratios
+    resized_data = keypoints.clone()
+    if torch._C._get_tracing_state():
+        resized_data_0 = resized_data[:, :, 0] * ratio_w
+        resized_data_1 = resized_data[:, :, 1] * ratio_h
+        resized_data = torch.stack((resized_data_0, resized_data_1, resized_data[:, :, 2]), dim=2)
+    else:
+        resized_data[..., 0] *= ratio_w
+        resized_data[..., 1] *= ratio_h
+    return resized_data
 
 
 def resize_boxes(boxes, original_size, new_size):
---
+++ zmain.py
@@ -0,0 +1,119 @@
+import cv2
+import numpy as np
+import random
+import torch
+
+coco_names = [
+    '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
+    'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+    'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
+    'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+    'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+    'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+    'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
+    'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+    'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
+    'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+]
+
+COLORS = np.random.uniform(0, 255, size=(len(coco_names), 3))
+
+
+def get_outputs(image, model, threshold):
+    with torch.no_grad():
+        # forward pass of the image through the modle
+        outputs = model(image)
+
+    # get all the scores
+    scores = list(outputs[0]['scores'].detach().cpu().numpy())
+    # index of those scores which are above a certain threshold
+    thresholded_preds_inidices = [scores.index(i) for i in scores if i > threshold]
+    thresholded_preds_count = len(thresholded_preds_inidices)
+    # get the masks
+    masks = (outputs[0]['masks'] > 0.5).squeeze().detach().cpu().numpy()
+    # discard masks for objects which are below threshold
+    masks = masks[:thresholded_preds_count]
+    # get the bounding boxes, in (x1, y1), (x2, y2) format
+    boxes = [[(i[0], i[1]), (i[2], i[3])] for i in outputs[0]['boxes'].detach().cpu()]
+    # discard bounding boxes below threshold value
+    boxes = boxes[:thresholded_preds_count]
+    # get the classes labels
+    labels = [coco_names[i] for i in outputs[0]['labels']]
+    return masks, boxes, labels
+
+
+def draw_segmentation_map(image, masks, boxes, labels):
+    alpha = 1
+    beta = 0.6  # transparency for the segmentation map
+    gamma = 0  # scalar added to each sum
+    for i in range(len(masks)):
+        red_map = np.zeros_like(masks[i]).astype(np.uint8)
+        green_map = np.zeros_like(masks[i]).astype(np.uint8)
+        blue_map = np.zeros_like(masks[i]).astype(np.uint8)
+        # apply a randon color mask to each object
+        color = COLORS[random.randrange(0, len(COLORS))]
+        red_map[masks[i] == 1], green_map[masks[i] == 1], blue_map[masks[i] == 1] = color
+        # combine all the masks into a single image
+        segmentation_map = np.stack([red_map, green_map, blue_map], axis=2)
+        # convert the original PIL image into NumPy format
+        image = np.array(image)
+        # convert from RGN to OpenCV BGR format
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        # apply mask on the image
+        cv2.addWeighted(image, alpha, segmentation_map, beta, gamma, image)
+        # draw the bounding boxes around the objects
+        cv2.rectangle(image, boxes[i][0], boxes[i][1], color=color,
+                      thickness=2)
+        # put the label text above the objects
+        cv2.putText(image, labels[i], (boxes[i][0][0], boxes[i][0][1] - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, color,
+                    thickness=2, lineType=cv2.LINE_AA)
+
+    return image
+
+import torch
+import torchvision
+import cv2
+import argparse
+from PIL import Image
+from torchvision.transforms import transforms as transforms
+from mask_rcnn import maskrcnn_resnet50_fpn
+
+# initialize the model
+# model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True, progress=True,
+#                                                            num_classes=91)
+model = maskrcnn_resnet50_fpn(pretrained=True, progress=True, num_classes=91)
+
+# set the computation device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# load the modle on to the computation device and set to eval mode
+model.to(device).eval()
+
+# transform to convert the image to tensor
+transform = transforms.Compose([
+    transforms.ToTensor()
+])
+
+# python mask_rcnn_images.py --input ../input/image1.jpg
+
+args_input = "image/cats.jpg"
+args_threshold = 0.965
+
+image_path = args_input
+image = Image.open(image_path).convert('RGB')
+# keep a copy of the original image for OpenCV functions and applying masks
+orig_image = image.copy()
+# transform the image
+image = transform(image)
+# add a batch dimension
+image = image.unsqueeze(0).to(device)
+masks, boxes, labels = get_outputs(image, model, args_threshold)
+result = draw_segmentation_map(orig_image, masks, boxes, labels)
+# visualize the image
+cv2.imshow('Segmented image', result)
+cv2.waitKey(0)
+# set the save path
+save_path = f"{args_input.split('/')[-1].split('.')[0]}.jpg"
+cv2.imwrite(save_path, result)
