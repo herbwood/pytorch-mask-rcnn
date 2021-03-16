09f1f4c91bbff5afe2cb7bec4fcaea52e191acee
June Kwon <herbwood27@gmail.com>
2021-03-16 21:44:53 +0900

    changed all2

--- backbone_utils.py
+++ backbone_utils.py
@@ -1,10 +1,9 @@
 import warnings
 from torch import nn
-from torchvision.ops.feature_pyramid_network import FeaturePyramidNetwork, LastLevelMaxPool
+from feature_pyramid_network import FeaturePyramidNetwork, LastLevelMaxPool
 
 from torchvision.ops import misc as misc_nn_ops
 from _utils import IntermediateLayerGetter
-import mobilenet
 import resnet
 
 
@@ -131,49 +130,3 @@ def _validate_trainable_layers(pretrained, trainable_backbone_layers, max_value,
         trainable_backbone_layers = default_value
     assert 0 <= trainable_backbone_layers <= max_value
     return trainable_backbone_layers
-
-
-def mobilenet_backbone(
-    backbone_name,
-    pretrained,
-    fpn,
-    norm_layer=misc_nn_ops.FrozenBatchNorm2d,
-    trainable_layers=2,
-    returned_layers=None,
-    extra_blocks=None
-):
-    backbone = mobilenet.__dict__[backbone_name](pretrained=pretrained, norm_layer=norm_layer).features
-
-    # Gather the indices of blocks which are strided. These are the locations of C1, ..., Cn-1 blocks.
-    # The first and last blocks are always included because they are the C0 (conv1) and Cn.
-    stage_indices = [0] + [i for i, b in enumerate(backbone) if getattr(b, "_is_cn", False)] + [len(backbone) - 1]
-    num_stages = len(stage_indices)
-
-    # find the index of the layer from which we wont freeze
-    assert 0 <= trainable_layers <= num_stages
-    freeze_before = num_stages if trainable_layers == 0 else stage_indices[num_stages - trainable_layers]
-
-    for b in backbone[:freeze_before]:
-        for parameter in b.parameters():
-            parameter.requires_grad_(False)
-
-    out_channels = 256
-    if fpn:
-        if extra_blocks is None:
-            extra_blocks = LastLevelMaxPool()
-
-        if returned_layers is None:
-            returned_layers = [num_stages - 2, num_stages - 1]
-        assert min(returned_layers) >= 0 and max(returned_layers) < num_stages
-        return_layers = {f'{stage_indices[k]}': str(v) for v, k in enumerate(returned_layers)}
-
-        in_channels_list = [backbone[stage_indices[i]].out_channels for i in returned_layers]
-        return BackboneWithFPN(backbone, return_layers, in_channels_list, out_channels, extra_blocks=extra_blocks)
-    else:
-        m = nn.Sequential(
-            backbone,
-            # depthwise linear combination of channels to reduce their size
-            nn.Conv2d(backbone[-1].out_channels, out_channels, 1),
-        )
-        m.out_channels = out_channels
-        return m
