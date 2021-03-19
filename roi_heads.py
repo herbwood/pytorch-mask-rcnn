12b7a82b0e2176199b240b8e859b68102bc8a71e
herbwood <herbwood27@gmail.com>
2021-03-19 19:46:59 +0900

    added examples"

--- roi_heads.py
+++ roi_heads.py
@@ -12,63 +12,9 @@ import _utils as det_utils
 from typing import Optional, List, Dict, Tuple
 
 
-def fastrcnn_loss(class_logits, box_regression, labels, regression_targets):
-    # type: (Tensor, Tensor, List[Tensor], List[Tensor]) -> Tuple[Tensor, Tensor]
-    """
-    Computes the loss for Faster R-CNN.
-
-    Args:
-        class_logits (Tensor)
-        box_regression (Tensor)
-        labels (list[BoxList])
-        regression_targets (Tensor)
-
-    Returns:
-        classification_loss (Tensor)
-        box_loss (Tensor)
-    """
-
-    labels = torch.cat(labels, dim=0)
-    regression_targets = torch.cat(regression_targets, dim=0)
-
-    classification_loss = F.cross_entropy(class_logits, labels)
-
-    # get indices that correspond to the regression targets for
-    # the corresponding ground truth labels, to be used with
-    # advanced indexing
-    sampled_pos_inds_subset = torch.where(labels > 0)[0]
-    labels_pos = labels[sampled_pos_inds_subset]
-    N, num_classes = class_logits.shape
-    box_regression = box_regression.reshape(N, box_regression.size(-1) // 4, 4)
-
-    box_loss = F.smooth_l1_loss(
-        box_regression[sampled_pos_inds_subset, labels_pos],
-        regression_targets[sampled_pos_inds_subset],
-        beta=1 / 9,
-        reduction='sum',
-    )
-    box_loss = box_loss / labels.numel()
-
-    return classification_loss, box_loss
-
 
 def maskrcnn_inference(x, labels):
-    # type: (Tensor, List[Tensor]) -> List[Tensor]
-    """
-    From the results of the CNN, post process the masks
-    by taking the mask corresponding to the class with max
-    probability (which are of fixed size and directly output
-    by the CNN) and return the masks in the mask field of the BoxList.
 
-    Args:
-        x (Tensor): the mask logits
-        labels (list[BoxList]): bounding boxes that are used as
-            reference, one for ech image
-
-    Returns:
-        results (list[BoxList]): one BoxList for each image, containing
-            the extra field mask
-    """
     mask_prob = x.sigmoid()
 
     # select masks corresponding to the predicted classes
@@ -82,78 +28,8 @@ def maskrcnn_inference(x, labels):
     return mask_prob
 
 
-def project_masks_on_boxes(gt_masks, boxes, matched_idxs, M):
-    # type: (Tensor, Tensor, Tensor, int) -> Tensor
-    """
-    Given segmentation masks and the bounding boxes corresponding
-    to the location of the masks in the image, this function
-    crops and resizes the masks in the position defined by the
-    boxes. This prepares the masks for them to be fed to the
-    loss computation as the targets.
-    """
-    matched_idxs = matched_idxs.to(boxes)
-    rois = torch.cat([matched_idxs[:, None], boxes], dim=1)
-    gt_masks = gt_masks[:, None].to(rois)
-    return roi_align(gt_masks, rois, (M, M), 1.)[:, 0]
-
-
-def maskrcnn_loss(mask_logits, proposals, gt_masks, gt_labels, mask_matched_idxs):
-    # type: (Tensor, List[Tensor], List[Tensor], List[Tensor], List[Tensor]) -> Tensor
-    """
-    Args:
-        proposals (list[BoxList])
-        mask_logits (Tensor)
-        targets (list[BoxList])
-
-    Return:
-        mask_loss (Tensor): scalar tensor containing the loss
-    """
-
-    discretization_size = mask_logits.shape[-1]
-    labels = [gt_label[idxs] for gt_label, idxs in zip(gt_labels, mask_matched_idxs)]
-    mask_targets = [
-        project_masks_on_boxes(m, p, i, discretization_size)
-        for m, p, i in zip(gt_masks, proposals, mask_matched_idxs)
-    ]
-
-    labels = torch.cat(labels, dim=0)
-    mask_targets = torch.cat(mask_targets, dim=0)
-
-    # torch.mean (in binary_cross_entropy_with_logits) doesn't
-    # accept empty tensors, so handle it separately
-    if mask_targets.numel() == 0:
-        return mask_logits.sum() * 0
-
-    mask_loss = F.binary_cross_entropy_with_logits(
-        mask_logits[torch.arange(labels.shape[0], device=labels.device), labels], mask_targets
-    )
-    return mask_loss
-
-def _onnx_expand_boxes(boxes, scale):
-    # type: (Tensor, float) -> Tensor
-    w_half = (boxes[:, 2] - boxes[:, 0]) * .5
-    h_half = (boxes[:, 3] - boxes[:, 1]) * .5
-    x_c = (boxes[:, 2] + boxes[:, 0]) * .5
-    y_c = (boxes[:, 3] + boxes[:, 1]) * .5
-
-    w_half = w_half.to(dtype=torch.float32) * scale
-    h_half = h_half.to(dtype=torch.float32) * scale
-
-    boxes_exp0 = x_c - w_half
-    boxes_exp1 = y_c - h_half
-    boxes_exp2 = x_c + w_half
-    boxes_exp3 = y_c + h_half
-    boxes_exp = torch.stack((boxes_exp0, boxes_exp1, boxes_exp2, boxes_exp3), 1)
-    return boxes_exp
-
-
-# the next two functions should be merged inside Masker
-# but are kept here for the moment while we need them
-# temporarily for paste_mask_in_image
 def expand_boxes(boxes, scale):
-    # type: (Tensor, float) -> Tensor
-    if torchvision._is_tracing():
-        return _onnx_expand_boxes(boxes, scale)
+
     w_half = (boxes[:, 2] - boxes[:, 0]) * .5
     h_half = (boxes[:, 3] - boxes[:, 1]) * .5
     x_c = (boxes[:, 2] + boxes[:, 0]) * .5
@@ -170,19 +46,10 @@ def expand_boxes(boxes, scale):
     return boxes_exp
 
 
-@torch.jit.unused
-def expand_masks_tracing_scale(M, padding):
-    # type: (int, int) -> float
-    return torch.tensor(M + 2 * padding).to(torch.float32) / torch.tensor(M).to(torch.float32)
-
 
 def expand_masks(mask, padding):
-    # type: (Tensor, int) -> Tuple[Tensor, float]
     M = mask.shape[-1]
-    if torch._C._get_tracing_state():  # could not import is_tracing(), not sure why
-        scale = expand_masks_tracing_scale(M, padding)
-    else:
-        scale = float(M + 2 * padding) / M
+    scale = float(M + 2 * padding) / M
     padded_mask = F.pad(mask, (padding,) * 4)
     return padded_mask, scale
 
@@ -211,58 +78,8 @@ def paste_mask_in_image(mask, box, im_h, im_w):
     im_mask[y_0:y_1, x_0:x_1] = mask[
         (y_0 - box[1]):(y_1 - box[1]), (x_0 - box[0]):(x_1 - box[0])
     ]
+
     return im_mask
-
-
-def _onnx_paste_mask_in_image(mask, box, im_h, im_w):
-    one = torch.ones(1, dtype=torch.int64)
-    zero = torch.zeros(1, dtype=torch.int64)
-
-    w = (box[2] - box[0] + one)
-    h = (box[3] - box[1] + one)
-    w = torch.max(torch.cat((w, one)))
-    h = torch.max(torch.cat((h, one)))
-
-    # Set shape to [batchxCxHxW]
-    mask = mask.expand((1, 1, mask.size(0), mask.size(1)))
-
-    # Resize mask
-    mask = F.interpolate(mask, size=(int(h), int(w)), mode='bilinear', align_corners=False)
-    mask = mask[0][0]
-
-    x_0 = torch.max(torch.cat((box[0].unsqueeze(0), zero)))
-    x_1 = torch.min(torch.cat((box[2].unsqueeze(0) + one, im_w.unsqueeze(0))))
-    y_0 = torch.max(torch.cat((box[1].unsqueeze(0), zero)))
-    y_1 = torch.min(torch.cat((box[3].unsqueeze(0) + one, im_h.unsqueeze(0))))
-
-    unpaded_im_mask = mask[(y_0 - box[1]):(y_1 - box[1]),
-                           (x_0 - box[0]):(x_1 - box[0])]
-
-    # TODO : replace below with a dynamic padding when support is added in ONNX
-
-    # pad y
-    zeros_y0 = torch.zeros(y_0, unpaded_im_mask.size(1))
-    zeros_y1 = torch.zeros(im_h - y_1, unpaded_im_mask.size(1))
-    concat_0 = torch.cat((zeros_y0,
-                          unpaded_im_mask.to(dtype=torch.float32),
-                          zeros_y1), 0)[0:im_h, :]
-    # pad x
-    zeros_x0 = torch.zeros(concat_0.size(0), x_0)
-    zeros_x1 = torch.zeros(concat_0.size(0), im_w - x_1)
-    im_mask = torch.cat((zeros_x0,
-                         concat_0,
-                         zeros_x1), 1)[:, :im_w]
-    return im_mask
-
-
-@torch.jit._script_if_tracing
-def _onnx_paste_masks_in_image_loop(masks, boxes, im_h, im_w):
-    res_append = torch.zeros(0, im_h, im_w)
-    for i in range(masks.size(0)):
-        mask_res = _onnx_paste_mask_in_image(masks[i][0], boxes[i], im_h, im_w)
-        mask_res = mask_res.unsqueeze(0)
-        res_append = torch.cat((res_append, mask_res))
-    return res_append
 
 
 def paste_masks_in_image(masks, boxes, img_shape, padding=1):
@@ -271,10 +88,6 @@ def paste_masks_in_image(masks, boxes, img_shape, padding=1):
     boxes = expand_boxes(boxes, scale).to(dtype=torch.int64)
     im_h, im_w = img_shape
 
-    if torchvision._is_tracing():
-        return _onnx_paste_masks_in_image_loop(masks, boxes,
-                                               torch.scalar_tensor(im_h, dtype=torch.int64),
-                                               torch.scalar_tensor(im_w, dtype=torch.int64))[:, None]
     res = [
         paste_mask_in_image(m[0], b, im_h, im_w)
         for m, b in zip(masks, boxes)
@@ -355,106 +168,6 @@ class RoIHeads(nn.Module):
             return False
         return True
 
-    def assign_targets_to_proposals(self, proposals, gt_boxes, gt_labels):
-        # type: (List[Tensor], List[Tensor], List[Tensor]) -> Tuple[List[Tensor], List[Tensor]]
-        matched_idxs = []
-        labels = []
-        for proposals_in_image, gt_boxes_in_image, gt_labels_in_image in zip(proposals, gt_boxes, gt_labels):
-
-            if gt_boxes_in_image.numel() == 0:
-                # Background image
-                device = proposals_in_image.device
-                clamped_matched_idxs_in_image = torch.zeros(
-                    (proposals_in_image.shape[0],), dtype=torch.int64, device=device
-                )
-                labels_in_image = torch.zeros(
-                    (proposals_in_image.shape[0],), dtype=torch.int64, device=device
-                )
-            else:
-                #  set to self.box_similarity when https://github.com/pytorch/pytorch/issues/27495 lands
-                match_quality_matrix = box_ops.box_iou(gt_boxes_in_image, proposals_in_image)
-                matched_idxs_in_image = self.proposal_matcher(match_quality_matrix)
-
-                clamped_matched_idxs_in_image = matched_idxs_in_image.clamp(min=0)
-
-                labels_in_image = gt_labels_in_image[clamped_matched_idxs_in_image]
-                labels_in_image = labels_in_image.to(dtype=torch.int64)
-
-                # Label background (below the low threshold)
-                bg_inds = matched_idxs_in_image == self.proposal_matcher.BELOW_LOW_THRESHOLD
-                labels_in_image[bg_inds] = 0
-
-                # Label ignore proposals (between low and high thresholds)
-                ignore_inds = matched_idxs_in_image == self.proposal_matcher.BETWEEN_THRESHOLDS
-                labels_in_image[ignore_inds] = -1  # -1 is ignored by sampler
-
-            matched_idxs.append(clamped_matched_idxs_in_image)
-            labels.append(labels_in_image)
-        return matched_idxs, labels
-
-    def subsample(self, labels):
-        # type: (List[Tensor]) -> List[Tensor]
-        sampled_pos_inds, sampled_neg_inds = self.fg_bg_sampler(labels)
-        sampled_inds = []
-        for img_idx, (pos_inds_img, neg_inds_img) in enumerate(
-            zip(sampled_pos_inds, sampled_neg_inds)
-        ):
-            img_sampled_inds = torch.where(pos_inds_img | neg_inds_img)[0]
-            sampled_inds.append(img_sampled_inds)
-        return sampled_inds
-
-    def add_gt_proposals(self, proposals, gt_boxes):
-        # type: (List[Tensor], List[Tensor]) -> List[Tensor]
-        proposals = [
-            torch.cat((proposal, gt_box))
-            for proposal, gt_box in zip(proposals, gt_boxes)
-        ]
-
-        return proposals
-
-    def check_targets(self, targets):
-        # type: (Optional[List[Dict[str, Tensor]]]) -> None
-        assert targets is not None
-        assert all(["boxes" in t for t in targets])
-        assert all(["labels" in t for t in targets])
-        if self.has_mask():
-            assert all(["masks" in t for t in targets])
-
-    def select_training_samples(self,
-                                proposals,  # type: List[Tensor]
-                                targets     # type: Optional[List[Dict[str, Tensor]]]
-                                ):
-        # type: (...) -> Tuple[List[Tensor], List[Tensor], List[Tensor], List[Tensor]]
-        self.check_targets(targets)
-        assert targets is not None
-        dtype = proposals[0].dtype
-        device = proposals[0].device
-
-        gt_boxes = [t["boxes"].to(dtype) for t in targets]
-        gt_labels = [t["labels"] for t in targets]
-
-        # append ground-truth bboxes to propos
-        proposals = self.add_gt_proposals(proposals, gt_boxes)
-
-        # get matching gt indices for each proposal
-        matched_idxs, labels = self.assign_targets_to_proposals(proposals, gt_boxes, gt_labels)
-        # sample a fixed proportion of positive-negative proposals
-        sampled_inds = self.subsample(labels)
-        matched_gt_boxes = []
-        num_images = len(proposals)
-        for img_id in range(num_images):
-            img_sampled_inds = sampled_inds[img_id]
-            proposals[img_id] = proposals[img_id][img_sampled_inds]
-            labels[img_id] = labels[img_id][img_sampled_inds]
-            matched_idxs[img_id] = matched_idxs[img_id][img_sampled_inds]
-
-            gt_boxes_in_image = gt_boxes[img_id]
-            if gt_boxes_in_image.numel() == 0:
-                gt_boxes_in_image = torch.zeros((1, 4), dtype=dtype, device=device)
-            matched_gt_boxes.append(gt_boxes_in_image[matched_idxs[img_id]])
-
-        regression_targets = self.box_coder.encode(matched_gt_boxes, proposals)
-        return proposals, matched_idxs, labels, regression_targets
 
     def postprocess_detections(self,
                                class_logits,    # type: Tensor
@@ -537,12 +250,10 @@ class RoIHeads(nn.Module):
                 if self.has_keypoint():
                     assert t["keypoints"].dtype == torch.float32, 'target keypoints must of float type'
 
-        if self.training:
-            proposals, matched_idxs, labels, regression_targets = self.select_training_samples(proposals, targets)
-        else:
-            labels = None
-            regression_targets = None
-            matched_idxs = None
+
+        labels = None
+        regression_targets = None
+        matched_idxs = None
 
         box_features = self.box_roi_pool(features, proposals, image_shapes)
         box_features = self.box_head(box_features)
@@ -550,25 +261,17 @@ class RoIHeads(nn.Module):
 
         result: List[Dict[str, torch.Tensor]] = []
         losses = {}
-        if self.training:
-            assert labels is not None and regression_targets is not None
-            loss_classifier, loss_box_reg = fastrcnn_loss(
-                class_logits, box_regression, labels, regression_targets)
-            losses = {
-                "loss_classifier": loss_classifier,
-                "loss_box_reg": loss_box_reg
-            }
-        else:
-            boxes, scores, labels = self.postprocess_detections(class_logits, box_regression, proposals, image_shapes)
-            num_images = len(boxes)
-            for i in range(num_images):
-                result.append(
-                    {
-                        "boxes": boxes[i],
-                        "labels": labels[i],
-                        "scores": scores[i],
-                    }
-                )
+
+        boxes, scores, labels = self.postprocess_detections(class_logits, box_regression, proposals, image_shapes)
+        num_images = len(boxes)
+        for i in range(num_images):
+            result.append(
+                {
+                    "boxes": boxes[i],
+                    "labels": labels[i],
+                    "scores": scores[i],
+                }
+            )
 
         if self.has_mask():
             mask_proposals = [p["boxes"] for p in result]
@@ -593,24 +296,11 @@ class RoIHeads(nn.Module):
                 raise Exception("Expected mask_roi_pool to be not None")
 
             loss_mask = {}
-            if self.training:
-                assert targets is not None
-                assert pos_matched_idxs is not None
-                assert mask_logits is not None
 
-                gt_masks = [t["masks"] for t in targets]
-                gt_labels = [t["labels"] for t in targets]
-                rcnn_loss_mask = maskrcnn_loss(
-                    mask_logits, mask_proposals,
-                    gt_masks, gt_labels, pos_matched_idxs)
-                loss_mask = {
-                    "loss_mask": rcnn_loss_mask
-                }
-            else:
-                labels = [r["labels"] for r in result]
-                masks_probs = maskrcnn_inference(mask_logits, labels)
-                for mask_prob, r in zip(masks_probs, result):
-                    r["masks"] = mask_prob
+            labels = [r["labels"] for r in result]
+            masks_probs = maskrcnn_inference(mask_logits, labels)
+            for mask_prob, r in zip(masks_probs, result):
+                r["masks"] = mask_prob
 
             losses.update(loss_mask)
 
